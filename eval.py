3ca7797c88d6f6c2f6a60b51e14dc6fa713da46b
Yehor Smoliakov <yehors+gh@ukr.net>
2022-12-07 14:17:03 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -6,10 +6,10 @@ from evaluate import load
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 from datasets import load_dataset
 
-model = WhisperForConditionalGeneration.from_pretrained(".....")
-processor = WhisperProcessor.from_pretrained("......")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
+processor = WhisperProcessor.from_pretrained("openai/whisper-small")
 
-test_set = load_dataset("csv", data_files='/home/yehor/ext-disk/whisper-ukrainian/data/testset.csv',  cache_dir='./data/cache')['train']
+test_set = load_dataset("csv", data_files='/home/ubuntu/data/test.csv',  cache_dir='./data/cache')['train']
 
 
 def map_to_pred(batch):
@@ -26,6 +26,9 @@ def map_to_pred(batch):
 
     batch['text'] = processor.tokenizer._normalize(batch['text'])
     batch["transcription"] = transcription
+    
+    print('Ground truth:', batch["transcription"])
+    print('Predicted text:', batch["text"])
 
     return batch
 
